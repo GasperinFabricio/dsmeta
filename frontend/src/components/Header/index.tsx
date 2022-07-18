a8dd3e65a2b40b60e781ee8e6750dd8bae3b1a11
Fabricio Gasperin <fabricio.alencar@soulasalle.com.br>
2022-07-18 17:43:26 -0300

    Changes

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -8,7 +8,7 @@ function Header() {
                 <h1>DSMeta</h1>
                 <p>
                     Desenvolvido por
-                    <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+                    <a href="https://www.instagram.com/devsuperior.ig">Fabricio Gasperin</a>
                 </p>
             </div>
         </header>
